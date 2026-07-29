65301a834f405c23e4011b9078e1946e1d28d711
Jay-hash223 <jayaram.denken@gmail.com>
2026-07-29 14:58:54 +0530

    Add RelationalOps.py

---
+++ RelationalOps.py
@@ -0,0 +1,28 @@
+# >< != ==
+
+# Examples:
+
+if 5>2:
+    print()
+    print("2 is greter than 5")
+    print("Rama Sita")
+
+    a = 91
+    b = 91
+    c = a*b
+    print("The value of c is",c)
+
+
+
+    value1 = 5
+    value2 = 22
+
+    if value1>value2:
+        print("value1 is greater")
+        print("value1 is",value1, "value2 is ",value2)
+
+    if value1<value2:
+        print("value1 is leser")
+        print("value1 is ",value1, "value2 is",value2)
+
+
