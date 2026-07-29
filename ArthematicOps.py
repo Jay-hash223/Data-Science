a02911b92d5c7683127b5d2c6ca6b36df6d45bc0
Jay-hash223 <jayaram.denken@gmail.com>
2026-07-29 14:46:53 +0530

    Add ArthematicOps.py

---
+++ ArthematicOps.py
@@ -0,0 +1,43 @@
+# Arthematic Operators are + - / %
+# Data Types are int,char,string,boolean,float
+
+a = 5
+b = 14
+
+def add():
+    a = a+b
+    print(a)
+    return a
+
+
+def sub():
+    a = a-b
+    print(a)
+    return a
+
+def multi():
+    a = a*b
+    print(a)
+    return a
+
+
+
+result = a + b
+print("a+b", result)
+
+result = a - b
+print("a-b", result)
+
+result = a * b
+print("a*b", result)
+
+result = a**b
+print("a**b", result)
+a=11
+b=2
+result = a % b  
+print("a%b", result)
+print(
+    "The % symbol in Python is called the Modulo Operator. It returns the remainder of dividing the left hand operand by right hand operand"
+)
+print("Whats does // does ???????/")
